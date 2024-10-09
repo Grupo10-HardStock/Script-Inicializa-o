9938155579f1a517a2b67e066c14b387ae5d26b2
PedroSouzaSptech <pedro.bsouza@sptech.school>
2024-10-09 10:17:55 -0300

    finalizado

--- SistemaOperacional.sh
+++ SistemaOperacional.sh
@@ -2,11 +2,9 @@
 
 echo "Meu primeiro shell script"
 
-# Atualizando o sistema
 echo "Atualizando o Linux antes de começarmos"
 sudo apt update && sudo apt upgrade -y
 
-# Verificando a versão do Python
 echo "Verificando a versão do Python"
 python3 --version
 
@@ -21,7 +19,6 @@ else
     fi 
 fi 
 
-# Verificando a versão do Docker
 echo "Verificando a versão do Docker"
 docker --version
 if [ $? = 0 ]; then 
@@ -34,17 +31,14 @@ else
         sudo apt install -y docker-ce 
     fi 
 fi 
-
-# Iniciando o serviço do Docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker login
 
-# Nome da imagem e tag
-docker_image="pedrobarbosasouza/imagem_node"
-docker_tag="tagname"  # Modifique essa tag conforme necessário
 
-# Pull da imagem Docker
+docker_image="pedrobarbosasouza/imagem_node"
+docker_tag="tagname"  
+
 echo "Fazendo o pull da imagem Docker: $docker_image:$docker_tag"
 if sudo docker pull "$docker_image:$docker_tag"; then
     echo "Imagem $docker_image:$docker_tag baixada com sucesso"
@@ -53,7 +47,6 @@ else
     exit 1
 fi
 
-# Executando o container Docker
 echo "Rodando a imagem Docker"
 if sudo docker run -d --name meu_container -p 3333:3333 "$docker_image:$docker_tag"; then
     echo "Container iniciado com sucesso"
@@ -61,14 +54,6 @@ else
     echo "Erro ao iniciar o container"
 fi
 
-# Verificando containers
 sudo docker ps -a
 
-# Docker push para o repositório Docker Hub
-echo "Fazendo push da imagem Docker para o repositório: $docker_image:$docker_tag"
-if sudo docker push "$docker_image:$docker_tag"; then
-    echo "Imagem $docker_image:$docker_tag enviada com sucesso para o Docker Hub"
-else
-    echo "Erro ao enviar a imagem $docker_image:$docker_tag para o Docker Hub"
-    exit 1
-fi
+
